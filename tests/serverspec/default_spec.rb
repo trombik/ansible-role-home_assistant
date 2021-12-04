3ddcaa27b279279b207aaa956b4f4e1554a138c4
Tomoyuki Sakurai <y@trombik.org>
2021-12-04 20:43:25 +0700

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,54 +1,29 @@
 require "spec_helper"
 require "serverspec"
 
-package = "home-assistant"
-service = "home-assistant"
-config  = "/etc/home-assistant/home-assistant.conf"
-user    = "home-assistant"
-group   = "home-assistant"
-ports   = [PORTS]
-log_dir = "/var/log/home-assistant"
-db_dir  = "/var/lib/home-assistant"
-
-case os[:family]
-when "freebsd"
-  config = "/usr/local/etc/home-assistant.conf"
-  db_dir = "/var/db/home-assistant"
-end
-
-describe package(package) do
-  it { should be_installed }
-end
+package = "homeassistant"
+user    = "hass"
+group   = user
+ports   = [8123]
+home = "/usr/home/#{user}"
+config = "#{home}/.homeassistant/configuration.yaml"
+venv_dir = "#{home}"
+venv_bin_dir = "#{venv_dir}/bin"
 
 describe file(config) do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("home-assistant") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
+  # it { should be_mode  }
   it { should be_owned_by user }
   it { should be_grouped_into group }
 end
 
-describe file(db_dir) do
+describe file venv_bin_dir do
   it { should exist }
+  it { should be_directory }
   it { should be_mode 755 }
   it { should be_owned_by user }
   it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/home-assistant") do
-    it { should be_file }
-  end
-end
-
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
 end
 
 ports.each do |p|
